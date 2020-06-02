0223f901f9b9eec981c6e98d7123861e4358e006
Anna Lizon <annakatarina1994@gmail.com>
2020-06-02 11:48:10 -0400

    videos displaying, finished styling

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
-  state = { videos: []};
+  state = { videos: [],
+            selectedVideo: null
+  };
+
+  componentDidMount() {
+    this.onTermSubmit("news");
+  }
 
   onTermSubmit = async searchTerm => {
     const response = await youtube.get("/search", {
@@ -12,14 +19,33 @@ class App extends React.Component {
         q: searchTerm
       }
     });
-      this.setState({ videos: response.data.items});
+      this.setState({ 
+        videos: response.data.items,
+        selectedVideo: response.data.items[0]
+      });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video })
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit}/>
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+            <VideoList 
+              onVideoSelect={this.onVideoSelect} 
+              videos={this.state.videos} 
+            />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
